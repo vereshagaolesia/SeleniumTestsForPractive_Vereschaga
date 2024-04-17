1decedc5d5d4113d9233b5ea048335615f64e9cb
ov.vereschaga <vereshaga.lesika@gmail.com>
2024-04-17 21:38:25 +0500

    дописала 2 и 3 тест, начала писать 4

--- SeleniumTests_Vereschaga1/SeleniumTestsForPractice.cs
+++ SeleniumTests_Vereschaga1/SeleniumTestsForPractice.cs
@@ -13,73 +13,82 @@ public class SeleniumTestsForPractice
 { 
     public ChromeDriver driver;
     
+    [SetUp]
+        public void SignIn()
     
+        { 
+            var options = new ChromeOptions();
+            options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+            
+            driver = new ChromeDriver(options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); 
+            driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
+            //  ввести логин и пароль
+            IWebElement login = driver.FindElement(By.Id("Username"));
+            login.SendKeys("vereshaga.lesika@gmail.com");
+            IWebElement password = driver.FindElement(By.Name("Password"));
+            password.SendKeys("Lolesik2024!");
+            IWebElement buttonSignIn = driver.FindElement(By.Name("button"));
+            buttonSignIn.Click();
+         }
+
     [Test]
     public void Authorization()
     
-    { 
-        var options = new ChromeOptions();
-        options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
-        
-        //  зайти в Хром (с помощью вебдрайвера)
-        driver = new ChromeDriver(options);
-        
-        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); // неявное ожидание
-        
-        //  перейти по урлу  
-        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
-        // IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10)); 
-        // IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("input[name='Username']")));
-        
-        //  ввести логин и пароль
-        var login = driver.FindElement(By.Id("Username"));
-        login.SendKeys("vereshaga.lesika@gmail.com");
-        var password = driver.FindElement(By.Name("Password"));
-        password.SendKeys("Lolesik2024!");
-        
-        //  нажать кнопку "Войти"
-        var enter = driver.FindElement(By.Name("button"));
-        enter.Click();
-       
-        // - проверяем что мы находимся на нужной странице
-        var news = driver.FindElement(By.CssSelector("[data-tid='Feed']"));
-        var currentUrl = driver.Url;
-        //Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news",
-        //"current url = " + currentUrl + " а должен быть https://staff-testing.testkontur.ru/news");
-
+    {
+        IWebElement news = driver.FindElement(By.CssSelector("[data-tid='Feed']"));
+        news.Should().NotBeNull();
+        string currentUrl = driver.Url;
         currentUrl.Should().Be("https://staff-testing.testkontur.ru/news");
-        
-        
-        
-        // Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news");
-        //Assert.That(driver.FindElement(By.Name("Новости")));
-        //typeof(Assert).Name("Новости");
-        
-        //  закрываем браузер (процесс драйвера)
-        
-        //currentUrl.Should().Be("https://staff-testing.testkontur.ru/news");
-
-        // закрываем браузер и убиваем процесс драйвера
-        driver.Quit();
-
     }
     
-    [Test]
+    // - проверяем что мы находимся на нужной странице
     
-    public void Community()
-    {
-        Authorization();
-       
-        // - проверяем что мы находимся на нужной странице
-        var news = driver.FindElement(By.CssSelector("[data-tid='Feed']"));
-        var currentUrl = driver.Url;
-        Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news",
-            "current url = " + currentUrl + " а должен быть https://staff-testing.testkontur.ru/news");
+    //Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news",
+    //"current url = " + currentUrl + " а должен быть https://staff-testing.testkontur.ru/news");
         
+    // IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10)); 
+    // IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("input[name='Username']")));
 
+    
+    [Test]  // 2. диалоги
+    public void Dialogs()
+    {   
+        //     - найти "Диалоги"
+        IWebElement buttonMessages = driver.FindElements(By.CssSelector("a[data-tid='Messages']"))[0];
+        //     - клик на "Диалоги"
+        buttonMessages.Click();
+        //     - проверить "диалоги" и урл
+        IWebElement dialog = driver.FindElement(By.CssSelector("h1[data-tid='Title']"));
+        dialog.Should().NotBeNull();
+        string urlDialogs = driver.Url;
+        urlDialogs.Should().Be("https://staff-testing.testkontur.ru/messages");
+        
+    }
 
+    [Test] // 3. личная страница
+           // (нужно редактировать тесты, если используются другие логин и пароль)
+    public void MyProfile()
+    {
+        // - найти иконку
+        IWebElement dropDownMenu = driver.FindElement(By.CssSelector("div[data-tid='Avatar']"));
+        // - клик на иконку
+        dropDownMenu.Click();
+        // - найти кнопку "мой профиль"
+        IWebElement myProfile = driver.FindElement(By.CssSelector("span[data-tid='Profile']"));
+        // - клик "мой профиль" (скрыто)
+        myProfile.Click();
+        // - проверяем урл
+        string urlMyProfile = driver.Url;
+        urlMyProfile.Should().Be("https://staff-testing.testkontur.ru/profile/0baa364d-2e32-47f8-9e88-7f3ac4233064");
+    }
 
-
+    [Test] // 4. поиск по имени (закрыть поиск)
+    public void MyProfile()  
+    // - найти поле ввода имени
+    // - ввести ""
+    // - проверить
+    
     [TearDown]
     public void TearDown()
     { 
--- SeleniumTests_Vereschaga1/obj/Debug/net8.0/SeleniumTests_Vereschaga1.AssemblyInfo.cs
+++ SeleniumTests_Vereschaga1/obj/Debug/net8.0/SeleniumTests_Vereschaga1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumTests_Vereschaga1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b63aba033c9202d6b2be4aa9bc05c65e7ac71f17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b93dd9807007d9dd836db26e9feb4811fa0de8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumTests_Vereschaga1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumTests_Vereschaga1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
