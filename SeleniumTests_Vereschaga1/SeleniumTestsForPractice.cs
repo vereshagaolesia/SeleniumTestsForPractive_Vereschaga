5581eba5543f455f4d53b15bd2f82c0b7a128824
ov.vereschaga <vereshaga.lesika@gmail.com>
2024-04-19 00:30:37 +0500

    дописала 6 тест, создала и почти закончила 4 и 5 тесты

--- SeleniumTests_Vereschaga1/SeleniumTestsForPractice.cs
+++ SeleniumTests_Vereschaga1/SeleniumTestsForPractice.cs
@@ -2,7 +2,6 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.DevTools.V122.FedCm;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 
@@ -11,7 +10,8 @@ namespace SeleniumTests_Vereschaga1;
 public class SeleniumTestsForPractice
 
 { 
-    public ChromeDriver driver;
+    private ChromeDriver driver;
+    private WebDriverWait wait;
     
     [SetUp]
         public void SignIn()
@@ -30,10 +30,12 @@ public class SeleniumTestsForPractice
             password.SendKeys("Lolesik2024!");
             IWebElement buttonSignIn = driver.FindElement(By.Name("button"));
             buttonSignIn.Click();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("[data-tid='Feed']")));
          }
 
-    [Test]
-    public void Authorization()
+    [Test]  //  1. Авторизация
+    public void CheckNews()
     
     {
         IWebElement news = driver.FindElement(By.CssSelector("[data-tid='Feed']"));
@@ -42,31 +44,24 @@ public class SeleniumTestsForPractice
         currentUrl.Should().Be("https://staff-testing.testkontur.ru/news");
     }
     
-    // - проверяем что мы находимся на нужной странице
     
-    //Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news",
-    //"current url = " + currentUrl + " а должен быть https://staff-testing.testkontur.ru/news");
-        
-    // IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10)); 
-    // IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("input[name='Username']")));
-
-    
-    [Test]  // 2. диалоги
+    [Test]  // 2. Переход в диалоги (ГОТОВО)
     public void Dialogs()
     {   
         //     - найти "Диалоги"
         IWebElement buttonMessages = driver.FindElements(By.CssSelector("a[data-tid='Messages']"))[0];
         //     - клик на "Диалоги"
         buttonMessages.Click();
-        //     - проверить "диалоги" и урл
-        IWebElement dialog = driver.FindElement(By.CssSelector("h1[data-tid='Title']"));
-        dialog.Should().NotBeNull();
+        //     - проверить заголовок "Диалоги"
+        string dialog = driver.FindElement(By.CssSelector("h1[data-tid='Title']")).Text;
+        dialog.Should().Be("Диалоги");
+        //     - проверитьи урл
         string urlDialogs = driver.Url;
         urlDialogs.Should().Be("https://staff-testing.testkontur.ru/messages");
         
     }
 
-    [Test] // 3. личная страница
+    [Test] // 3. личная страница   (ГОТОВО)
            // (нужно редактировать тесты, если используются другие логин и пароль)
     public void MyProfile()
     {
@@ -80,36 +75,107 @@ public class SeleniumTestsForPractice
         urlMyProfile.Should().Be("https://staff-testing.testkontur.ru/profile/0baa364d-2e32-47f8-9e88-7f3ac4233064");
     }
 
-    [Test] // 4. поиск по имени (закрыть поиск)
-    public void fg()
+    [Test]   // 4. Создать сообщество
+     public void CreateCommunity()
     {
+     // - переход по урл на https://staff-testing.testkontur.ru/communities
+     driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/communities");
+     // - найти и кликнуть на кнопку "Создать" Xpath //*[@id="root"]/section/section[2]/section/div[2]/span/button/svg
+     IWebElement buttonCreate = driver.FindElement(By.CssSelector("section[data-tid='PageHeader']")).FindElement(By.TagName("button"));
+     buttonCreate.Click();
+     // - найти и заполнить название data-tid="Name"
+     IWebElement nameCommunity = driver.FindElement(By.CssSelector("textarea[placeholder='Название сообщества']"));
+     nameCommunity.SendKeys("1newCommunity");
+     // - найти и кликнуть на кнопку создать
+     IWebElement buttonCreateCommynity = driver.FindElement(By.CssSelector("span[data-tid='CreateButton']"));
+     buttonCreateCommynity.Click();
+     // - найти и кликнуть на ссылку со своим названием
+     
+     // - проверить, содержит ли data-tid="Title"
+        
+        
     }
-    // - найти поле ввода имени
-    // - ввести ""
-    // - проверить
+     
+     
+    [Test]   // 5. Удалить сообщество
+    public void DeleteCommunity()
+    {
+        // - метод создать сообщество
+        CreateCommunity();
+        // - переход по урл в список сообществ
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/communities");
+        // - найти кнопку "Я модератор"
+        IWebElement buttonModerator = driver.FindElements(By.CssSelector("a[data-tid='Item']"))[2];
+        // - клик по "Я модератор"
+        buttonModerator.Click();   
+        // - ждать
+        
+        // - найти сообщество div title="" data-tid="Avatar"  или a data-tid="Link"
+        IWebElement community = driver.FindElement(By.CssSelector("a[data-tid='Item']"));
+        // - клик
+        community.Click();
+        // - найти меню  PopupMenu__caption
+        IWebElement dropMenu = driver.FindElements(By.CssSelector("a[data-tid='PopupMenu__caption']"))[1];
+        // - клик
+        dropMenu.Click();
+        // - найти кнопку "Настройки"
+        IWebElement settings = driver.FindElement(By.CssSelector("a[data-tid='Settings']"));
+        // - клик
+        settings.Click();
+        // - найти "Удалить сообщество" button DeleteButton
+        IWebElement buttonDelete = driver.FindElement(By.CssSelector("button[data-tid='DeleteButton']"));
+        // - клик
+        buttonDelete.Click();
+        // - найти кнопку "Удалить" (подтвердить удаление)
+        IWebElement buttonDelete2 = driver.FindElement(By.CssSelector("button[data-tid='DeleteButton']"));
+        // - клик
+        buttonDelete2.Click();
+        // - проверить урл "Новости" https://staff-testing.testkontur.ru/news
+        CheckNews();
+
+
+    }
     
-    [Test] // 6. создать папку в Файлы (hard)
+
+    [Test] // 6. создать папку в Файлы
     public void AddFolder()
     {
         // - переход по урл на https://staff-testing.testkontur.ru/files
         driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/files");
-        // - клик "добавить"
-        IWebElement buttonAdd = driver.FindElements(By.CssSelector("span[data-tid='DropdownButton']"))[1];
+        // - найти и кликнуть "добавить"
+        wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div[data-tid='Title']")));
+        IWebElement checkPage = driver.FindElement(By.CssSelector("div[data-tid='Title']"));
+        checkPage.Should().NotBeNull();
+        IWebElement buttonAdd = driver.FindElement(By.CssSelector("button[type='button']"));
         buttonAdd.Click();
-        // - клик "папку" (скрыто)
+        // - найти и кликнуть "папку"
          IWebElement buttonFolder = driver.FindElement(By.CssSelector("span[data-tid='CreateFolder']"));
          buttonFolder.Click();
-        // - ввести название папки 
-        IWebElement newFolder = driver.FindElement(By.CssSelector("label[data-tid='Input']"));
-        newFolder.SendKeys("1NewFolder");
+        // - ввести название папки
+        IWebElement createFolder = driver.FindElement(By.CssSelector("input[placeholder='Новая папка']"));
+        createFolder.SendKeys("1NewFolder");
         // - найти активную кнопку и клик "сохранить"
-        IWebElement buttonSave = driver.FindElement(typeof(By("button")); // доделать!
+        IWebElement buttonSave = driver.FindElement(By.CssSelector("span[data-tid='SaveButton']"));
         buttonSave.Click();
         // - проверить название папки на странице
+        IWebElement newFolder = driver.FindElement(By.XPath("//*[contains(text(),'1NewFolder')]"));
+        
+        //string dialog = driver.FindElement(By.CssSelector("h1[data-tid='Title']")).Text;
+        //dialog.Should().Be("Диалоги");
         
     }
     
-    
+    //Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news",
+    //"current url = " + currentUrl + " а должен быть https://staff-testing.testkontur.ru/news");
+        
+    // IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10)); 
+    // IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("input[name='Username']")));
+
+    // IWebElement getToNewCommunity =
+    //     driver.FindElement(By.XPath("//*[@id=\"root\"]/section/section[2]/section/span/a"));
+    // getToNewCommunity.Click();
+    // string checkName = driver.FindElement(By.CssSelector("div[data-tid='Title']")).Text;
+    // checkName.Should().Be(name);
     
     [TearDown]
     public void TearDown()
--- SeleniumTests_Vereschaga1/obj/Debug/net8.0/SeleniumTests_Vereschaga1.AssemblyInfo.cs
+++ SeleniumTests_Vereschaga1/obj/Debug/net8.0/SeleniumTests_Vereschaga1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumTests_Vereschaga1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1decedc5d5d4113d9233b5ea048335615f64e9cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b9db216dfbebc3292dd2026acbc4f7ba37f72a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumTests_Vereschaga1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumTests_Vereschaga1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
